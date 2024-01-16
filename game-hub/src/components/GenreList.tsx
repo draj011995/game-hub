762321855a7b4dda3f0a537a6fcf8a0a24f9fb8e
deepak raj <rajtarvedi@gmail.com>
2024-01-16 22:09:56 +1000

    Highligh the selected genre

--- game-hub/src/components/GenreList.tsx
+++ game-hub/src/components/GenreList.tsx
@@ -5,9 +5,10 @@ import GenreSkeleton from "./GenreSkeleton";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
@@ -30,6 +31,7 @@ const GenreList = ({ onSelectGenre }: Props) => {
                 variant="link"
                 whiteSpace="normal"
                 textAlign="left"
+                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
                 onClick={() => onSelectGenre(genre)}
               >
                 {genre.name}
