b989c963909a7977e3a37c39c4fdc5a39615eaef
deepak raj <rajtarvedi@gmail.com>
2024-01-19 23:15:38 +1000

    Clean up the genres

--- game-hub/src/components/GenreList.tsx
+++ game-hub/src/components/GenreList.tsx
@@ -1,4 +1,11 @@
-import { Button, HStack, Image, List, ListItem } from "@chakra-ui/react";
+import {
+  Button,
+  HStack,
+  Heading,
+  Image,
+  List,
+  ListItem,
+} from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/images-url";
 import GenreSkeleton from "./GenreSkeleton";
@@ -17,6 +24,9 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
       {error && null}
       {isLoading &&
         skeletons.map((skeleton) => <GenreSkeleton key={skeleton} />)}
+      <Heading fontSize="2xl" marginBottom={3}>
+        Genres
+      </Heading>
       <List>
         {data.map((genre) => (
           <ListItem key={genre.id} paddingY="5px">
@@ -24,6 +34,7 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
               <Image
                 boxSize="32px"
                 borderRadius={8}
+                objectFit="cover"
                 src={getCroppedImageUrl(genre.image_background)}
               />
               <Button
